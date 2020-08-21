fea8a56dcc51b7b6087e28468514ab7d742b969c
shubham dixit <shubhamd.monter@gmail.com>
2020-08-21 18:40:06 +0530

    add bio and onBoarding in pratice schema

---
+++ doctor/practicemeta.model.js
@@ -0,0 +1,10 @@
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const practiceMeta= new Schema({
+//     practiceId:{type:Schema.Types.ObjectId , ref:"Practise"},
+//     recentPatient:[{
+//         patient:{type:Schema.Types.ObjectId,ref:"Patient"},
+
+//     }]
+// });
